962668bd1d522fcb85af71e1769c5e0871143a8b
eduardo <eduardomendes418@gmail.com>
2025-08-16 15:19:22 -0300

    [B2B-03] feat: create clerk

---
+++ apps/web/app/test/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <div>test page</div>;
+};
+
+export default Page;
--- packages/backend/convex/users.ts
+++ packages/backend/convex/users.ts
@@ -1,9 +1,25 @@
-import { query } from "./_generated/server"
+import { query, mutation } from "./_generated/server";
 
 export const getMany = query({
-    args: {},
-    handler: async (ctx) => {
-        const users = await ctx.db.query("users").collect();
-        return users;
-    },
+  args: {},
+  handler: async (ctx) => {
+    const users = await ctx.db.query("users").collect();
+    return users;
+  },
+});
+
+export const add = mutation({
+  args: {},
+  handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (identity === null) {
+      throw new Error("Not authenticated");
+    }
+
+    const userId = await ctx.db.insert("users", {
+      name: "Eduardo",
+    });
+
+    return userId;
+  },
 });
